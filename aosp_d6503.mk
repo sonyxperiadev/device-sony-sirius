2bb1b91ed2cb0e1fec26ba0fc61f8c43458ebe0e
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2016-02-07 12:51:44 +0100

    cleanup and consolidate include/inherit

--- aosp_d6503.mk
+++ aosp_d6503.mk
@@ -17,15 +17,11 @@ TARGET_KERNEL_CONFIG := aosp_shinano_sirius_defconfig
 DEVICE_PACKAGE_OVERLAYS += \
     device/sony/sirius/overlay
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony.mk)
-$(call inherit-product, device/sony/common/common.mk)
 $(call inherit-product, device/sony/shinano/device.mk)
 $(call inherit-product, vendor/sony/sirius/sirius-vendor.mk)
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 $(call inherit-product, hardware/broadcom/wlan/bcmdhd/config/config-bcm.mk)
-$(call inherit-product-if-exists, prebuilts/chromium/webview_prebuilt.mk)
-$(call inherit-product-if-exists, vendor/google/products/gms.mk)
 
 PRODUCT_COPY_FILES += \
     device/sony/sirius/rootdir/system/etc/audio_policy.conf:system/etc/audio_policy.conf \
