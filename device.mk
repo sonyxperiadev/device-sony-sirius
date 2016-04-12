c061653cbbe77067bcfcb93a4d198d4bc1c3d604
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2016-04-12 09:44:08 +0200

    sensor cfg cleanup
    move sensor configs to etc/sensors
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- device.mk
+++ device.mk
@@ -19,7 +19,7 @@ PRODUCT_COPY_FILES := \
     device/sony/sirius/rootdir/system/etc/audio_policy.conf:system/etc/audio_policy.conf \
     device/sony/sirius/rootdir/system/etc/BCM4339.hcd:system/etc/firmware/BCM43xx.hcd \
     device/sony/sirius/rootdir/system/etc/wifi/bcmdhd.cal:system/etc/wifi/bcmdhd.cal \
-    device/sony/sirius/rootdir/system/etc/sensor_def_qcomdev.conf:system/etc/sensor_def_qcomdev.conf \
+    device/sony/sirius/rootdir/system/etc/sensors/sensor_def_qcomdev.conf:system/etc/sensors/sensor_def_qcomdev.conf \
     device/sony/sirius/rootdir/system/etc/thermanager.xml:system/etc/thermanager.xml \
     device/sony/sirius/rootdir/system/etc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
     device/sony/sirius/rootdir/system/etc/libnfc-nxp.conf:system/etc/libnfc-nxp.conf \
