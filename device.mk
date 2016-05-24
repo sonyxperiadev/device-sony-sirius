f90781fc234f6e420047c7322e84555582ca726e
David <Davidteri91@gmail.com>
2016-05-24 23:47:57 +0200

    Sirius: Build InCallUI Signed-off-by: David Viteri <davidteri91@gmail.com>

--- device.mk
+++ device.mk
@@ -81,8 +81,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     nfc_nci.sirius
 
-# SimToolKit
+# Telephony Packages (AOSP)
 PRODUCT_PACKAGES += \
+    InCallUI \
     Stk
 
 PRODUCT_AAPT_CONFIG := large
